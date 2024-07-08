79c76fb1bccf35b67a1bef6f89e06842381a4291
Estefanía <estefaniaramirezmnt@gmail.com>
2024-07-08 11:43:00 +0200

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world</h1>
+      <h1>hello world</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/'/>
+          <Route path='/cart'/>
+        </Routes>
+      </Router>
     </div>
   );
 }
