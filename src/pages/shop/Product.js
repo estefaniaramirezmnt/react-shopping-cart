b11007c8d67c3be9a96d95fba94dcf4c2c6f81e8
Estefanía <estefaniaramirezmnt@gmail.com>
2024-07-08 14:03:47 +0200

    added folder for products and coded map for products

---
+++ src/pages/shop/Product.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Product(props) {
+    const {id, productName, price, producImate} = props.data;
+  return (
+    <div>Product</div>
+  )
+}
--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import { PRODUCTS } from "../../products";
+import Product from "./Product";
 
 export default function Shop() {
   return (
-    <div>Shop</div>
-  )
+    <div className="shop">
+      <h1>Shopping cart</h1>
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product data={product}/>
+        ))}
+      </div>
+    </div>
+  );
 }
