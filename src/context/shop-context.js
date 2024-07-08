ddfe493a71f9baf014020ac15741c18f21c59983
Estefanía <estefaniaramirezmnt@gmail.com>
2024-07-08 15:09:19 +0200

    added context file with addToCart and removeFromCart

---
+++ src/context/shop-context.js
@@ -0,0 +1,37 @@
+import React, { createContext, useState } from "react";
+import { PRODUCTS } from "../products";
+
+export const ShopContext = createContext(null);
+
+const getDefaultCart = () => {
+  let cart = {};
+  for (let i = 1; PRODUCTS.lenght + 1; i++) {
+    cart[i] = 0;
+  }
+  // this piece of code is equivalent to:
+  // cart = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, };
+  return cart;
+};
+
+export const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, itemId: prev[itemId] + 1 }));
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, itemId: prev[itemId] - 1 }));
+  };
+
+  const contextValue = {
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
