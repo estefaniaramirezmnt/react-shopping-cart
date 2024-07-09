5988fbcf0169038d55a48b2c57c32cc176be7022
Estefanía <estefaniaramirezmnt@gmail.com>
2024-07-09 10:52:57 +0200

    added css for cartItem

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -3,6 +3,8 @@ import { PRODUCTS } from "../../products";
 import { ShopContext } from "../../context/shop-context";
 import CartItem from "./CartItem";
 
+import "./cart.css";
+
 export default function Cart() {
   const { cartItems } = useContext(ShopContext);
   return (
