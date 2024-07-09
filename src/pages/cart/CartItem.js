79013b92bbc20ab8a058fc5a01fa985f3a661a28
Estefanía <estefaniaramirezmnt@gmail.com>
2024-07-09 11:02:52 +0200

    eliminated index.css

--- src/pages/cart/CartItem.js
+++ src/pages/cart/CartItem.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
 
 export default function CartItem(props) {
   const { id, productName, price, productImage } = props.data;
+  const { cartItems } = useContext(ShopContext);
+
   return (
     <div className="cartItem">
       <img src={productImage} />
@@ -10,6 +13,11 @@ export default function CartItem(props) {
           <b> {productName} </b>
         </p>
         <p>{price}</p>
+        <div className="countHandler">
+            <button>-</button>
+            <input value={cartItems[id]}/>
+            <button>+</button>
+        </div>
       </div>
     </div>
   );
